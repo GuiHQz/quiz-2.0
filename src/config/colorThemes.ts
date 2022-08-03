46e1fcd29c3c253242be84d6d7050bb8da8253b7
GuiHQz <guilher.queiroz@gmail.com>
2022-08-02 22:12:29 -0300

    feat: Adição de arquivos de temas

---
+++ src/config/colorThemes.ts
@@ -0,0 +1,18 @@
+export const color = {
+    //Cores padrões
+    white: "#FFFFFF",
+    black: "#000000",
+    lightBlue: "#93E9FA",
+
+    //Cores das equipes
+    boxes: {
+        orange: "#F5AA35",
+        blue: "#1296E0",
+    },
+    hover: {
+        boxes: {
+            orange: "#FFA00D",
+            blue: "#0569A1",
+        },
+    },
+}
