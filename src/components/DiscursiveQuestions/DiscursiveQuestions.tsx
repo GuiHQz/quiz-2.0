210ec9586fd8547a4a5585bc524c8aa4397163b5
GuiHQz <guilher.queiroz@gmail.com>
2022-08-04 21:52:31 -0300

    feat: Ajuste componente DiscursiveQuestions

---
+++ src/components/DiscursiveQuestions/DiscursiveQuestions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Drawer } from "@mui/material";
+
+export const DiscursiveQuestions: React.FC = () => {
+    return(
+        <React.Fragment>
+            <Drawer>
+
+            </Drawer>
+
+            
+        </React.Fragment>
+    )
+}
---
+++ src/components/DiscursiveQuestions/index.ts
@@ -0,0 +1,1 @@
+export { DiscursiveQuestions } from "./DiscursiveQuestions";
