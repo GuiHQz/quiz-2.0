1c3319a891a6f6a4ea62ef06e2adb7e1aa5d0d60
GuiHQz <guilher.queiroz@gmail.com>
2022-08-05 23:33:41 -0300

    chore: Ajuste arquivos do PageOne

---
+++ src/features/inititalPage/pages/PageOne/PageOne.controller.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { PageOneView } from "./PageOne.view";
+
+export const PageOne: React.FC = () => {
+    return(
+        <PageOneView />
+    )
+}
