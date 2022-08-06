2682ce4ee943bada648dc048251853bd4bbf71a8
GuiHQz <guilher.queiroz@gmail.com>
2022-08-05 23:34:27 -0300

    chore: Ajuste App correção novo nome Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import { Questions } from './features/inititalPage/pages/Questions';
+import { PageOne } from './features/inititalPage/pages/PageOne';
 
 function App() {
   return (
-    <Questions />
+    <PageOne />
   );
 }
 
