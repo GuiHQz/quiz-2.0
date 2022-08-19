df0cf333e8755dea50dd6119bb901fed69e42ed8
GuiHQz <guilher.queiroz@gmail.com>
2022-08-18 21:24:05 -0300

    feat: Subida arquivo de questões

---
+++ src/data/DiscursiveQuestions.ts
@@ -0,0 +1,52 @@
+export const DiscursiveQuestions = {
+    q1: {
+        numberQuestion: "01",
+        title: "Título pergunta número 1",
+        answer: "Resposta 1",
+    },
+    q2: {
+        numberQuestion: "02",
+        title: "Título pergunta número 2",
+        answer: "Resposta 2",
+    },
+    q3: {
+        numberQuestion: "03",
+        title: "Título pergunta número 3",
+        answer: "Resposta 3",
+    },
+    q4: {
+        numberQuestion: "04",
+        title: "Título pergunta número 4",
+        answer: "Resposta 4",
+    },
+    q5: {
+        numberQuestion: "05",
+        title: "Título pergunta número 5",
+        answer: "Resposta 5",
+    },
+    q6: {
+        numberQuestion: "06",
+        title: "Título pergunta número 6",
+        answer: "Resposta 6",
+    },
+    q7: {
+        numberQuestion: "07",
+        title: "Título pergunta número 7",
+        answer: "Resposta 7",
+    },
+    q8: {
+        numberQuestion: "08",
+        title: "Título pergunta número 8",
+        answer: "Resposta 8",
+    },
+    q9: {
+        numberQuestion: "09",
+        title: "Título pergunta número 9",
+        answer: "Resposta 9",
+    },
+    q10: {
+        numberQuestion: "10",
+        title: "Título pergunta número 10",
+        answer: "Resposta 10",
+    },
+}
