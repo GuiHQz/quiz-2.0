1780f08802355f38836baaab3687f9d48f6cb1f5
GuiHQz <guilher.queiroz@gmail.com>
2022-08-18 22:01:22 -0300

    chore: Adicionando funcionalidade props

--- src/components/DiscursiveQuestions/DiscursiveQuestions.tsx
+++ src/components/DiscursiveQuestions/DiscursiveQuestions.tsx
@@ -3,13 +3,19 @@ import { Drawer, Box, Button } from "@mui/material";
 import { useStyles } from "./DiscrusiveQuestions.style";
 
 interface DiscursiveQuestionsProps {
-  openQuestion: boolean
-  onClose: (value: boolean) => void
+  openQuestion: boolean;
+  onClose: (value: boolean) => void;
+  numberQuestion: string;
+  title?: string;
+  answer?: string;
 }
 
 const DiscursiveQuestions: React.FC<DiscursiveQuestionsProps> = ({
   openQuestion,
   onClose,
+  numberQuestion,
+  title,
+  answer,
 }) => {
   const styles = useStyles();
 
@@ -24,10 +30,14 @@ const DiscursiveQuestions: React.FC<DiscursiveQuestionsProps> = ({
         onClose={() => onClose(false)}
       >
         <Box className={styles.content}>
-          <Box>
             <Button
               onClick={onCloseQuestion}
               className={styles.closeButton}>Fechar</Button>
+          <Box className={styles.title}>
+            <span>{numberQuestion}. {title}</span>
+          </Box>
+          <Box className={styles.title}>
+            <span>{answer}</span>
           </Box>
         </Box>
       </Drawer>
--- src/features/inititalPage/pages/PageOne/PageOne.controller.tsx
+++ src/features/inititalPage/pages/PageOne/PageOne.controller.tsx
@@ -1,47 +1,60 @@
 import React from "react";
 import { PageOneView } from "./PageOne.view";
+import { DiscursiveQuestions } from "data/DiscursiveQuestions";
 
 const PageOne: React.FC = () => {
   const [openQuestion, setOpenQuestion] = React.useState(false);
+  const [numberQuestion, setNumberQuestion] = React.useState(' ');
+  const questions = DiscursiveQuestions;
 
   const firstQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q1.numberQuestion);
   };
 
   const secondQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q2.numberQuestion);
   };
 
   const thirdQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q3.numberQuestion);
   };
 
   const fourthQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q4.numberQuestion);
   };
 
   const fifthQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q5.numberQuestion);
   };
 
   const sixthQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q6.numberQuestion);
   };
 
   const sevethQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q7.numberQuestion);
   };
 
   const eighthQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q8.numberQuestion);
   };
 
   const ninthQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q9.numberQuestion);
   };
 
   const tenthQuestion = () => {
     setOpenQuestion(true);
+    setNumberQuestion(questions.q10.numberQuestion);
   };
 
   const onCloseQuestion = () => {
@@ -62,6 +75,7 @@ const PageOne: React.FC = () => {
       tenthQuestion={tenthQuestion}
       openQuestion={openQuestion}
       onClose={onCloseQuestion}
+      numberQuestion={numberQuestion}
     />
   );
 };
